a6d2362d45d515a96f251e531a48ec6fca0d79d7
jg585Username <88526510+jg585Username@users.noreply.github.com>
2022-09-23 23:11:57 -0700

    Update Main.java

--- DataAbstractionbank/src/Main.java
+++ DataAbstractionbank/src/Main.java
@@ -1,20 +1,18 @@
-import java.text.DateFormat;
-import java.time.Instant;
-import java.util.Calendar;
+
 import java.util.Date;
-public class Main { //display
+public class Main { 
 
     public static void main(String[] args) {
-        Customer demo = new Customer(); //lets the customer know nothing has been done at this time (filler)
-        demo.deposit(400, Customer.CHECKING);
-        demo.deposit(500, Customer.SAVING);
-        demo.displayDeposits();
-        System.out.println("Updating activity...");
-        System.out.println();
-        demo.withdraw(400, Customer.CHECKING);
-        demo.withdraw(500, Customer.SAVING);
-        demo.displayWithdraws();
+        //displaying
+        Deposit checkingDeposit = new Deposit(500.0, new Date(), "Checking");
+        Deposit savingDeposit = new Deposit(200.0, new Date(), "Saving");
+        System.out.println(checkingDeposit);
+        System.out.println(savingDeposit);
+        
+        Withdraw checkingWithdraw = new Withdraw(500.0, new Date(), "Checking");
+        Withdraw savingWithdraw = new Withdraw(200.0, new Date(), "Saving");
+        System.out.println(checkingWithdraw);
+        System.out.println(savingWithdraw);
         //In the rubric it didn't say date needed to be changeable so I did not do that, instead just used current date for simplicity
-
     }
 }
