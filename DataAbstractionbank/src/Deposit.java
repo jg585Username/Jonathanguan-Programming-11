e6548753d3dde76e8f47422b35cc79acb8323e1f
jg585Username <88526510+jg585Username@users.noreply.github.com>
2022-09-23 23:28:17 -0700

    Update Deposit.java

--- DataAbstractionbank/src/Deposit.java
+++ DataAbstractionbank/src/Deposit.java
@@ -12,18 +12,24 @@ public class Deposit {
     }
 
     @Override
+    //Requires: nothing
+    //Modifies: nothing
+    //Effect: strings/puts together important information to display (Deposits)
     public String toString(){
         String sentence = "Deposit of: $" + amount + " ";
         String sentence1 = "Date: " + date + " ";
         String sentence2 = "Into account: " + account + " ";
         return sentence + sentence1 + sentence2;
     }
-
+    //Requires: nothing
+    //Modifies: nothing
+    //Effects: returns amount for overdraft method
     public double getAmount() {
         return amount;
     }
-
-
+    //Requires: nothing
+    //Modifies: nothing
+    //Effects: returns account for overdraft method
     public String getAccount() {
         return account;
     }
